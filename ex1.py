eb03eb3a3750a2c181d5ce93f874c4b98819c9fe
dookidooki <bombtol@gmail.com>
2022-08-27 14:18:00 +0900

    오늘은 2일차

---
+++ ex1.py
@@ -0,0 +1,8 @@
+def func1():
+    return "모듈에 있는 첫번째 함수 입니다"
+def func2():
+    return "모듈에 있는 두번째 함수 입니다"
+def func3():
+    return "모듈에 있는 세번째 함수 입니다"
+def func4():
+    return "모듈에 있는 네번째 함수 입니다"
